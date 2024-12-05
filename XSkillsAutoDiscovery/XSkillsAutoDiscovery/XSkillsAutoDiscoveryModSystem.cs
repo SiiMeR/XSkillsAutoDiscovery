843ff3a0dcaecf8bd155ea834f2343cc571555fc
Siim Raudsepp <siimraudsepp@msn.com>
2024-12-05 17:09:23 +0200

    Add XSkillsSaplingBehavior to saplings

--- XSkillsAutoDiscovery/XSkillsAutoDiscovery/XSkillsAutoDiscoveryModSystem.cs
+++ XSkillsAutoDiscovery/XSkillsAutoDiscovery/XSkillsAutoDiscoveryModSystem.cs
@@ -1,28 +1,34 @@
 ﻿using System.Linq;
 using Vintagestory.API.Common;
 using Vintagestory.API.Datastructures;
+using Vintagestory.GameContent;
 using XSkills;
 
 namespace XSkillsAutoDiscovery;
 
 public class XSkillsAutoDiscoveryModSystem : ModSystem
 {
+    private ICoreAPI _api;
+
     public override void AssetsFinalize(ICoreAPI api)
     {
         if (api.Side != EnumAppSide.Server) return;
 
-        AutomapWoodBlocks(api);
+        _api = api;
+
+
+        AutomapWoodBlocks();
+        AutomapSaplings();
 
         base.AssetsFinalize(api);
     }
 
-    private void AutomapWoodBlocks(ICoreAPI api)
-    {
-         var woodBlocks = api.World.Blocks.Where(block =>
-            block.Class == "BlockLog" && block.Code.PathStartsWith("log-grown") &&
-            block.BlockBehaviors.All(behavior => behavior is not XSkillsWoodBehavior));
 
-        // TODO: log-section
+    private void AutomapWoodBlocks()
+    {
+        var woodBlocks = _api.World.Blocks.Where(block =>
+            block.Class == nameof(BlockLog) && block.Code.PathStartsWith("log-grown") &&
+            block.BlockBehaviors.All(behavior => behavior is not XSkillsWoodBehavior));
 
         foreach (var block in woodBlocks)
         {
@@ -30,7 +36,23 @@ public class XSkillsAutoDiscoveryModSystem : ModSystem
             woodBehavior.Initialize(JsonObject.FromJson("{\"xp\": 0.4}"));
 
             block.BlockBehaviors = block.BlockBehaviors.Append(woodBehavior).ToArray();
-            api.Logger.Debug($"Added XSkillsWoodBehavior to {block.Code.Path}");
+            _api.Logger.Debug($"Added {nameof(XSkillsWoodBehavior)} to {block.Code.Path}");
+        }
+    }
+
+    private void AutomapSaplings()
+    {
+        var saplingBlocks = _api.World.Blocks.Where(block =>
+            block.EntityClass == "Sapling" && block.Code.PathStartsWith("sapling") &&
+            block.BlockBehaviors.All(behavior => behavior is not XSkillsSaplingBehavior));
+
+        foreach (var block in saplingBlocks)
+        {
+            var behavior = new XSkillsSaplingBehavior(block);
+            behavior.Initialize(JsonObject.FromJson("{}"));
+
+            block.BlockBehaviors = block.BlockBehaviors.Append(behavior).ToArray();
+            _api.Logger.Debug($"Added {nameof(XSkillsSaplingBehavior)} to {block.Code.Path}");
         }
     }
 }
