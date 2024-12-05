9c6f827e2729a957d3a905c74d877080ae4b564e
Siim Raudsepp <siimraudsepp@msn.com>
2024-12-05 23:19:31 +0200

    More automappings

--- XSkillsAutoDiscovery/XSkillsAutoDiscovery/XSkillsAutoDiscoveryModSystem.cs
+++ XSkillsAutoDiscovery/XSkillsAutoDiscovery/XSkillsAutoDiscoveryModSystem.cs
@@ -17,27 +17,66 @@ public class XSkillsAutoDiscoveryModSystem : ModSystem
         _api = api;
 
 
+        // forestry
         AutomapWoodBlocks();
         AutomapSaplings();
+        AutomapTreeCuttings();
+        AutomapLeaves();
+
+
+        AutomapOres();
+        AutomapGems();
+        AutomapBombs();
+
+        AutomapBerryBushes();
 
         base.AssetsFinalize(api);
     }
 
+    private void AutomapBerryBushes()
+    {
+        var berryBlocks = _api.World.Blocks.Where(block =>
+            block.Class == "BlockBerryBush" &&
+            block.BlockBehaviors.All(behavior => behavior is not XSkillsBerryBushBehavior) &&
+            block.Code.Path.Contains("ripe"));
+
+        foreach (var block in berryBlocks)
+            AddBehaviorToBlock(block, new XSkillsBerryBushBehavior(block), "{\"xp\": 0.33}");
+    }
+
+    private void AutomapBombs()
+    {
+        var bombBlocks = _api.World.Blocks.Where(block =>
+            block.Class == "BlockBomb" && block.EntityClass == "Bomb" &&
+            block.BlockBehaviors.All(behavior => behavior is not XSkillsBombBehavior));
+
+        foreach (var block in bombBlocks) AddBehaviorToBlock(block, new XSkillsBombBehavior(block), "{}");
+    }
+
+    private void AutomapGems()
+    {
+        // TODO: How to separate gems from ores?
+    }
+
+    private void AutomapTreeCuttings()
+    {
+        // TODO: Figure out how tree cuttings work
+    }
 
     private void AutomapWoodBlocks()
     {
-        var woodBlocks = _api.World.Blocks.Where(block =>
+        var logBlocks = _api.World.Blocks.Where(block =>
             block.Class == nameof(BlockLog) && block.Code.PathStartsWith("log-grown") &&
             block.BlockBehaviors.All(behavior => behavior is not XSkillsWoodBehavior));
 
-        foreach (var block in woodBlocks)
-        {
-            var woodBehavior = new XSkillsWoodBehavior(block);
-            woodBehavior.Initialize(JsonObject.FromJson("{\"xp\": 0.4}"));
+        foreach (var block in logBlocks) AddBehaviorToBlock(block, new XSkillsLeavesBehavior(block), "{\"xp\": 0.4}");
 
-            block.BlockBehaviors = block.BlockBehaviors.Append(woodBehavior).ToArray();
-            _api.Logger.Debug($"Added {nameof(XSkillsWoodBehavior)} to {block.Code.Path}");
-        }
+        var logSectionBlocks = _api.World.Blocks.Where(block =>
+            block.Class == nameof(BlockLogSection) && block.Code.PathStartsWith("logsection-grown") &&
+            block.BlockBehaviors.All(behavior => behavior is not XSkillsWoodBehavior));
+
+        foreach (var block in logSectionBlocks)
+            AddBehaviorToBlock(block, new XSkillsWoodBehavior(block), "{\"xp\": 0.25}");
     }
 
     private void AutomapSaplings()
@@ -46,13 +85,60 @@ public class XSkillsAutoDiscoveryModSystem : ModSystem
             block.EntityClass == "Sapling" && block.Code.PathStartsWith("sapling") &&
             block.BlockBehaviors.All(behavior => behavior is not XSkillsSaplingBehavior));
 
-        foreach (var block in saplingBlocks)
-        {
-            var behavior = new XSkillsSaplingBehavior(block);
-            behavior.Initialize(JsonObject.FromJson("{}"));
+        foreach (var block in saplingBlocks) AddBehaviorToBlock(block, new XSkillsSaplingBehavior(block), "{}");
+    }
 
-            block.BlockBehaviors = block.BlockBehaviors.Append(behavior).ToArray();
-            _api.Logger.Debug($"Added {nameof(XSkillsSaplingBehavior)} to {block.Code.Path}");
-        }
+    private void AutomapLeaves()
+    {
+        // TODO: doesnt give xp for some reason even though the behavior is there
+
+        // BlockLeavesDropCanes is a patch class by Wildcraft
+
+        // normal and narrow leaves 0.01xp
+        var regularLeavesBlocks = _api.World.Blocks.Where(block =>
+            block.Class is "BlockLeavesDropCanes" or "BlockLeaves" or "BlockLeavesNarrow" &&
+            block.Code.PathStartsWith("leaves") &&
+            block.BlockBehaviors.All(behavior => behavior is not XSkillsLeavesBehavior));
+        foreach (var block in regularLeavesBlocks)
+            AddBehaviorToBlock(block, new XSkillsLeavesBehavior(block), "{\"xp\": 0.01}");
+
+        // branchy leaves 0.02xp
+        var branchyBlocks = _api.World.Blocks.Where(block =>
+            block.Class is "BlockLeavesDropCanes" or "BlockLeaves" &&
+            block.Code.PathStartsWith("branchyleaves") &&
+            block.BlockBehaviors.All(behavior => behavior is not XSkillsLeavesBehavior));
+
+        foreach (var block in branchyBlocks)
+            AddBehaviorToBlock(block, new XSkillsLeavesBehavior(block), "{\"xp\": 0.02}");
+    }
+
+    private void AutomapOres()
+    {
+        var oreBlocks = _api.World.Blocks.Where(block =>
+            block.Class == "BlockOre" && block.Code.PathStartsWith("ore"));
+
+        // TODO: verify this works
+        var properties = """
+                                 {
+                                   "xpByType": {
+                                     "*-poor-*": "0.5",
+                                     "*-medium-*": "0.7",
+                                     "*-rich-*": "0.9",
+                                     "*-bountiful-*": "1.0",
+                                     "*": "0.3"
+                                   }
+                                 }
+                         """;
+
+        foreach (var block in oreBlocks)
+            AddBehaviorToBlock(block, new XSkillsOreBehavior(block), properties);
+    }
+
+    private void AddBehaviorToBlock(Block block, BlockBehavior behavior, string properties)
+    {
+        behavior.Initialize(JsonObject.FromJson(properties));
+
+        block.BlockBehaviors = block.BlockBehaviors.Append(behavior).ToArray();
+        _api.Logger.Debug($"Added {behavior.GetType().Name} to {block.Code.Path}");
     }
 }
